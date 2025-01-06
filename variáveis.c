f465aebcc98be41759c0ef782684ac31c5303ceb
Speedydev77 <yanvasco492@gmail.com>
2025-01-06 15:26:41 -0300

    variáveis.c

---
+++ variáveis.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int idade = 22;
+    int quantidade = 1;
+    float altura = 1.76;
+    double peso = 70.7;
+    char letra = 'A';
+    char nome[20] = "speedy";
+
+idade = 23;
+quantidade = 10;
+altura = 1.79;
+peso = 130.4
+}
