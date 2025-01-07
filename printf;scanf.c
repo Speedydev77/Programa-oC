3f7d41a8ab758e2ab41793545b33248968e26d4c
Speedydev77 <yanvasco492@gmail.com>
2025-01-06 23:12:43 -0300

    Update printf.c

--- printf;scanf.c
+++ printf;scanf.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int main(){
+  // Passo 1: Declarar as variáveis int, float, char, double
+  int idade = 22;
+  float altura = 1.75;
+  char opcao = 'S'
+  char nome(20) = "speedy"
+
+  // Passo2: Usando o tipo de dado printf com o formato da impressão ("%d, %i, %f, %e, %c, %s", variavel1, variavel2, variavel3, variavel4, variavel5, variavel6)
+
+  
+   printf("altura: %f"\n, altura) // impressão de variável altura em uma linha única (\n)
+   printf("Opcao: %c"\n, opcao) // impressão de variável opcao em uma linha única (\n)
+   printf("nome: %s idade: %d", nome, idade) // impressão de variáveis nome e idade na mesma linha
+  
+  // sintaxe padrão printf 
+  // printf ("texto com formatação", variavel1, variavel2, ...);
+  printf ("Olá mundo 2!\n");
+  // \n escrever na próxima linha
+
+  /*
+  printf("%formato1 %formato2 %formato3", variavel1, variavel2, variavel3) 
+
+  %d: imprime um inteiro no formato decimal
+  %i: equivalente a %d
+  %f: imprime um número de ponto flutuante no formato padrão
+  %e: imprime um número de ponto flutuante em notação científica
+  %c: imprime um único caractere
+  %s: imprime uma cadeia (string) de caracteres
+  
+  */
+
+}
